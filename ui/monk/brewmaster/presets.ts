35af76c9304faa8dac528bd4399a9dbc865c545b
Adrian Klingen <adrian.klingen@deptagency.com>
2025-10-22 17:23:42 +0200

    Add P2 Offensive sets and optimise Sha APL

--- ui/monk/brewmaster/presets.ts
+++ ui/monk/brewmaster/presets.ts
@@ -1,6 +1,6 @@
 import { Encounter } from '../../core/encounter';
 import * as PresetUtils from '../../core/preset_utils';
-import { ConsumesSpec, Glyphs, Profession, PseudoStat, Spec, Stat } from '../../core/proto/common';
+import { ConsumesSpec, Glyphs, Profession, PseudoStat, Spec, Stat, Encounter as EncounterProto, Target as TargetProto } from '../../core/proto/common';
 import { BrewmasterMonk_Options as BrewmasterMonkOptions, MonkMajorGlyph, MonkMinorGlyph } from '../../core/proto/monk';
 import { SavedTalents } from '../../core/proto/ui';
 import { Stats } from '../../core/proto_utils/stats';
@@ -12,13 +12,17 @@ import GarajalBuild from './builds/garajal_default.build.json';
 import ShaBuild from './builds/sha_default.build.json';
 import P1BISDWGear from './gear_sets/p1_bis_dw.gear.json';
 import P2BISDWGear from './gear_sets/p2_bis_dw.gear.json';
+import P2BISOffensiveDWGear from './gear_sets/p2_bis_offensive_dw.gear.json';
+import P2BISOffensiveTierlessDWGear from './gear_sets/p2_bis_offensive_tierless_dw.gear.json';
 
 // Preset options for this spec.
 // Eventually we will import these values for the raid sim too, so its good to
 // keep them in a separate file.
 
-export const P1_BIS_DW_GEAR_PRESET = PresetUtils.makePresetGear('P1 BiS - Balanced', P1BISDWGear);
-export const P2_BIS_DW_GEAR_PRESET = PresetUtils.makePresetGear('P2 BiS - Balanced', P2BISDWGear);
+export const P1_BIS_DW_GEAR_PRESET = PresetUtils.makePresetGear('P1 - BIS (Balanced)', P1BISDWGear);
+export const P2_BIS_DW_GEAR_PRESET = PresetUtils.makePresetGear('P2 - BIS (Balanced)', P2BISDWGear);
+export const P2_BIS_OFFENSIVE_DW_GEAR_PRESET = PresetUtils.makePresetGear('P2 - BIS (Offensive)', P2BISOffensiveDWGear);
+export const P2_BIS_OFFENSIVE_TIERLESS_DW_GEAR_PRESET = PresetUtils.makePresetGear('P2 - BIS (Offensive - No Tier)', P2BISOffensiveTierlessDWGear);
 
 export const ROTATION_PRESET = PresetUtils.makePresetAPLRotation('Generic', DefaultApl);
 export const ROTATION_GARAJAL_PRESET = PresetUtils.makePresetAPLRotation("Gara'jal", GarajalApl);
@@ -26,26 +30,50 @@ export const ROTATION_OFFENSIVE_PRESET = PresetUtils.makePresetAPLRotation('Offe
 export const ROTATION_SHA_PRESET = PresetUtils.makePresetAPLRotation('Sha of Fear', ShaApl);
 
 // Preset options for EP weights
-export const PREPATCH_EP_PRESET = PresetUtils.makePresetEpWeights(
-	'Default',
+export const P1_BALANCED_EP_PRESET = PresetUtils.makePresetEpWeights(
+	'Balanced',
 	Stats.fromMap(
 		{
 			[Stat.StatAgility]: 1,
 			[Stat.StatStamina]: 0.81,
+			[Stat.StatHitRating]: 1.28,
+			[Stat.StatCritRating]: 0.67,
+			[Stat.StatHasteRating]: 0.42,
+			[Stat.StatExpertiseRating]: 0.96,
+			[Stat.StatDodgeRating]: 0.2,
+			[Stat.StatParryRating]: 0.23,
+			[Stat.StatMasteryRating]: 0.65,
+			[Stat.StatAttackPower]: 0.24,
 			[Stat.StatArmor]: 0.41,
 			[Stat.StatBonusArmor]: 0.41,
-			[Stat.StatAttackPower]: 0.24,
-			[Stat.StatCritRating]: 0.67,
-			[Stat.StatDodgeRating]: 0.20,
-			[Stat.StatParryRating]: 0.23,
-			[Stat.StatHitRating]: 1.28,
-			[Stat.StatExpertiseRating]: 0.96,
-			[Stat.StatHasteRating]: 0.42,
-			[Stat.StatMasteryRating]: 0.65,
 		},
 		{
-			[PseudoStat.PseudoStatMainHandDps]: 1.50,
+			[PseudoStat.PseudoStatMainHandDps]: 1.5,
 			[PseudoStat.PseudoStatOffHandDps]: 0.74,
+		},
+	),
+);
+
+export const P2_OFFENSIVE_EP_PRESET = PresetUtils.makePresetEpWeights(
+	'Offensive',
+	Stats.fromMap(
+		{
+			[Stat.StatAgility]: 1,
+			[Stat.StatStamina]: 0.27,
+			[Stat.StatHitRating]: 1.56,
+			[Stat.StatCritRating]: 0.7,
+			[Stat.StatHasteRating]: 0.26,
+			[Stat.StatExpertiseRating]: 1.2,
+			[Stat.StatDodgeRating]: 0.52,
+			[Stat.StatParryRating]: 0.22,
+			[Stat.StatMasteryRating]: 0.25,
+			[Stat.StatAttackPower]: 0.52,
+			[Stat.StatArmor]: 0.27,
+			[Stat.StatBonusArmor]: 0.26,
+		},
+		{
+			[PseudoStat.PseudoStatMainHandDps]: 1.33,
+			[PseudoStat.PseudoStatOffHandDps]: 0.66,
 		},
 	),
 );
@@ -92,6 +120,13 @@ export const DefaultConsumables = ConsumesSpec.create({
 	foodId: 74648, // Sea Mist Rice Noodles
 	prepotId: 76090, // Potion of the Mountains
 	potId: 76090, // Potion of the Mountains
+	conjuredId: 5512, // Healthstone
+});
+
+export const OffensiveConsumables = ConsumesSpec.create({
+	...DefaultConsumables,
+	prepotId: 76089, // Virmen's Bite
+	potId: 76089, // Virmen's Bite
 });
 
 export const OtherDefaults = {
@@ -102,4 +137,4 @@ export const OtherDefaults = {
 };
 
 export const PRESET_BUILD_GARAJAL = PresetUtils.makePresetBuildFromJSON("Gara'jal", Spec.SpecBrewmasterMonk, GarajalBuild);
-export const PRESET_BUILD_SHA = PresetUtils.makePresetBuildFromJSON("Sha of Fear", Spec.SpecBrewmasterMonk, ShaBuild);
+export const PRESET_BUILD_SHA = PresetUtils.makePresetBuildFromJSON('Sha of Fear', Spec.SpecBrewmasterMonk, ShaBuild);
